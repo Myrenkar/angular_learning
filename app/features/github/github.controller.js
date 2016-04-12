c8581cd24d6a19c4f20431be8b965b7300ba718e
myrenkar <piotr.torczynski@gmail.com>
2016-04-12 16:57:19 +0000

    Lista repozytoriow

--- app/features/github/github.controller.js
+++ app/features/github/github.controller.js
@@ -1,17 +1,27 @@
 export default class GitHubController {
   constructor($http) {
     this.$http = $http;
-    
     this.makeRequest();
+    this.getRepos()
   }
   
   makeRequest() {
     this.$http.get('https://api.github.com/users/Myrenkar')
       .then(this.assignToScope.bind(this));
   }
+
+  getRepos(){
+   this.$http.get('https://api.github.com/users/Myrenkar/repos')
+      .then(this.assignRepoToScope.bind(this));
+  }
+  
+  assignRepoToScope(response){
+    this.Repo = response.data;
+     console.log(this.Repo);
+  }
   
   assignToScope(response){
     this.User = response.data;
-    console.log(this.User)
+   // console.log(this.User);
   }
 }
